b23e2c50eb3673c3def1e02efe2e2a6e8ec862b9
jw88 <jw88@live.com>
2018-05-30 03:36:46 -0700

    u

---
+++ src/main/java/com/example/demo/client/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,16 @@
+package com.example.demo.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.model.CurrencyConversionBean;
+
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
+
+
